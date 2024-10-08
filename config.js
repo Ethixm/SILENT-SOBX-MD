78cd1893f038a88157118c45ad1eaa7bdcf0ba01
Ethixm <Ethixm@gmail.com>
2024-10-08 18:16:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
+SESSION_ID: process.env.SESSION_ID || "Qd4wXL7R#SwGqJEorP0zDB3e0-RWI_vYkYXwinJTEOL1Z7JCUY1s",
 MONGODB: process.env.MONGODB || "enter mongodb url",
 };
